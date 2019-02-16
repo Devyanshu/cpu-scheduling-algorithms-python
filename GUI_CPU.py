1fa2e6e37fdc0aafd0fd4941f76e1c2bd3d5afc8
Devyanshu Shukla <Devyanshu@users.noreply.github.com>
2019-02-16 22:09:24 +0530

    Formatted code

--- GUI_CPU.py
+++ GUI_CPU.py
@@ -6,10 +6,12 @@ root.title('Main Window')
 root.geometry('450x300')
 frame = Frame(root)
 
-frame.grid(row=0,column=0)
+frame.grid(row=0, column=0)
 prcs = None
-#Buttons
-def table_win(): # new window definition
+# Buttons
+
+
+def table_win():  # new window definition
     processes = int(prcs.get())
     newwin = Toplevel(root)
     b = Label(newwin, text="Process")
@@ -20,12 +22,13 @@ def table_win(): # new window definition
     b.grid(row=0, column=2)
     b = Label(newwin, text="Priority")
     b.grid(row=0, column=3)
-    for i in range(1,processes+1): #Rows
-        for j in range(4): #Columns
+    for i in range(1, processes+1):  # Rows
+        for j in range(4):  # Columns
             b = Entry(newwin, text="")
             b.grid(row=i, column=j)
-    sub = Button(newwin ,text="Submit").grid(row=processes+2,column=1)
-    Reset=Button(newwin,text="Reset").grid(row=processes+3,column=1)
+    sub = Button(newwin, text="Submit").grid(row=processes+2, column=1)
+    Reset = Button(newwin, text="Reset").grid(row=processes+3, column=1)
+
 
 Label(frame, text="Enter Processes").grid(row=0, column=1)
 prcs = Entry(frame, text="")
@@ -34,13 +37,13 @@ prcs.grid(row=0, column=2)
 Label(frame, text="").grid(row=2, column=1)
 Label(frame, text="").grid(row=3, column=1)
 Label(frame, text="").grid(row=4, column=1)
-generate=Button(frame,text="Generate",fg="black",font=('arial',8,'bold'),width=10,height=1,command=table_win).grid(row=5,column=0)
-compute=Button(frame,text="Compute",fg="black",font=('arial',8,'bold'),width=10,height=1).grid(row=5,column=2)
-exit_button=Button(frame,text="Exit",fg="black",font=('arial',8,'bold'),width=10,height=1,command=root.destroy).grid(row=5,column=3)
+generate = Button(frame, text="Generate", fg="black", font=(
+    'arial', 8, 'bold'), width=10, height=1, command=table_win).grid(row=5,
+                                                                     column=0)
+compute = Button(frame, text="Compute", fg="black", font=(
+    'arial', 8, 'bold'), width=10, height=1).grid(row=5, column=2)
+exit_button = Button(frame, text="Exit", fg="black", font=(
+    'arial', 8, 'bold'), width=10, height=1, command=root.destroy).grid(row=5,
+                                                                        column=3)
 
 root.mainloop()
-
-
-
-
-
