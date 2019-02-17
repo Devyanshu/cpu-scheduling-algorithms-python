7686f90b4e02118dfacaf88a627e657a34091efc
Devyanshu Shukla <Devyanshu@users.noreply.github.com>
2019-02-17 17:32:55 +0530

    Minor chages

---
+++ gen_chart.py
@@ -0,0 +1,14 @@
+# import plotly.graph_objs as go
+# import plotly
+import json
+data = []
+with open('data.json', "r") as read_file:
+    data = json.load(read_file)
+print(data)
+
+
+def generate_gantt(data):
+    for dct in 
+
+
+generate_gantt(data)
